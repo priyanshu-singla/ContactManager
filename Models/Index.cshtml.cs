9ebd37ce55bb2a5d7eea19c9cf88bafc81faf9fb
Priyanshu Singla <106403747+priyanshu-singla@users.noreply.github.com>
2022-08-07 20:00:48 -0400

    Initial commit

---
+++ Models/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContactManager.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ContactManager.Models
+{
+    //[AllowAnonymous]
+    public class IndexModel : PageModel
+    {
+        private readonly ContactManager.Data.ApplicationDbContext _context;
+
+        public IndexModel(ContactManager.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Contact> Contact { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Contact != null)
+            {
+                Contact = await _context.Contact.ToListAsync();
+            }
+        }
+    }
+}
